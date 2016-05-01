ae8d3efd03e686a4b656d4352f7789b06e75b440
katherinethai <kthai1618@gmail.com>
2016-04-30 22:57:25 -0400

    added ability to login and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,53 @@ class ApplicationController < Sinatra::Base
   get '/' do 
     erb :index
   end
-  
+
+  get '/login' do
+    if logged_in?
+      redirect '/books'
+    else
+      erb :'users/login'
+    end
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect '/books'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      erb :'users/create_user', locals: {message: "Please fill in all fields."}
+    else
+      user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      redirect '/books'
+    end
+  end
+
+  post '/login' do
+    if params[:username] == "" || params[:password] == ""
+      erb :'users/login', locals: {message: "Please enter your username and password."}
+    else
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:id] == user.id
+        redirect '/books'
+      else
+        erb :'users/login', locals: {message: "Incorrect username or password."}
+      end
+    end
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find(session[:id])
+    end
+  end
 end
